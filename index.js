992b1e6c69ac4abea1d8eaa018b6258f8e137020
nentq22 <anhtdph10189@fpt.edu.vn>
2021-04-01 22:28:42 +0700

    fix lỗi và hoàn thành

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var expressHBS = require('express-handlebars');
+
+var app =express();
+app.listen(process.env.PORT || 5000);
+app.use(express.static('css_new'));
+
+app.engine('.handlebars', expressHBS());
+
+app.set('view engine','.handlebars');
+app.get('', function (req,res){
+    res.render('index'); //file name
+});
+// đợi tôi tý dc
+//ok đc
+// móa nó ko lên css cay thật
+//tôi xem đang không hiểu đc đang àm gì luôn :)
+// để tôi fixx lại đc
+//ok đc
